3b4b88a15b719984a2a5f6353fcc54b79b758d2c
shalinia <shalini@servelots.com>
2015-08-29 15:06:55 +0530

    unique directories created to store extracted zip files

--- upload.php
+++ upload.php
@@ -1,10 +1,14 @@
 <?php
-if($_FILES["zip_file"]["name"]) {
+include_once("header.php");
+header("Content-Type: text/html");
+if(isset($_FILES["zip_file"])) {
 	$filename = $_FILES["zip_file"]["name"];
 	$source = $_FILES["zip_file"]["tmp_name"];
 	$type = $_FILES["zip_file"]["type"];
          	
 	$name = explode(".", $filename);
+ $target = '/var/www/html/bgpx/uploads/' . $name[0] . '-' . time() . '/';  
+        
 	$accepted_types = array('application/zip', 'application/x-zip-compressed', 'multipart/x-zip', 'application/x-compressed');
 	foreach($accepted_types as $mime_type) {
 		if($mime_type == $type) {
@@ -14,28 +18,41 @@ if($_FILES["zip_file"]["name"]) {
 	}
 	
 	$continue = strtolower($name[1]) == 'zip' ? true : false;
-	echo $name[1];
 	if(!$continue) {
-	//	$message = "The file you are trying to upload is not a .zip file. Please try again.";
 		echo 'The file you are trying to upload is not a .zip file. Please try again.';
 	}
-        else {
-	$target_path = "/var/www/html/bgpx/".$filename;  // change this to the correct site path
+
+	mkdir($target,0777,true);
+        $target_path = $target . $filename;
+
 	if(move_uploaded_file($source, $target_path)) {
 		$zip = new ZipArchive();
 		$x = $zip->open($target_path);
 		if ($x === true) {
-			$zip->extractTo("/var/www/html/bgpx/"); // change this to the correct site path
+		     	$zip->extractTo($target); // change this to the correct site path
 			$zip->close();
 	
 			unlink($target_path);
 		}
-		echo 'Your .zip file was uploaded and unpacked.';
+		echo 'Your zip file was uploaded and unpacked.';
 	} else {	
 		echo 'There was a problem with the upload. Please try again.';
 	}
-      }
+
+
+			$scan = scandir($target . $name[0]);
+
+			print '<ul';
+			for ($i=0; $i<count($scan); $i++)
+			{
+				echo '<li>' . $scan[$i] . '</li>';
+			}
+			print '</ul>';
+
 }
+
+
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
